1c7dd28befdddfc91faccdfb24ffe477d84500e7
haja-Tx <andrianantoaninahaja@gmail.com>
2022-08-31 19:13:15 +0300

    home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import Header from './components/header'
+import Footer from './components/footer'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+        <Head>
+          <title>OrderLock</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+          <Component {...pageProps} />
+        </main>
+        <Footer/>
+    </div>
+    </>
+    )
+  
 }
 
 export default MyApp
---
+++ pages/order/index.tsx
@@ -0,0 +1,5 @@
+function Index() {
+    return <div>About</div>
+  }
+  
+  export default Index
